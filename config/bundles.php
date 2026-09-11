ed5fa81837473335da2a75bc235834053010e7f0
Alexandru Cardos <alexandru.cardos@emag.ro>
2026-09-11 11:53:08 +0300

    [freebuff-test] request logger

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     AsyncAws\Symfony\Bundle\AsyncAwsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
